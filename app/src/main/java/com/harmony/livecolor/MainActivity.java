bc87404689e092f06b4f643fbe331764f4e8898b
Paige <spam.paige@gmail.com>
2019-10-07 19:49:17 -0700

    Initial commit

---
+++ app/src/main/java/com/harmony/livecolor/MainActivity.java
@@ -0,0 +1,14 @@
+package com.harmony.livecolor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
