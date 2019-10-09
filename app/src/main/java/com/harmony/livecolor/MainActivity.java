fe37491a7d0ecfa140ef1683d84b7cd8ebc807d0
Paige <spam.paige@gmail.com>
2019-10-08 18:45:02 -0700

    Added notes to main activity

--- app/src/main/java/com/harmony/livecolor/MainActivity.java
+++ app/src/main/java/com/harmony/livecolor/MainActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+// MAIN ACTIVITY - COLOR PICKER
+// [See the designs on our marvel for creating and implementing UI]
+// -- Display camera and gallery view (requires permissions for both)
+// -- Switch between the two with a radio button for each (styled like Instagram)
+// -- Color picking feature (we can probably use a bitmap to obtain the pixel and then grab the data)
 public class MainActivity extends AppCompatActivity {
 
     @Override
