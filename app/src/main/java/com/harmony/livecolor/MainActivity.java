73f3ee1bd25763a192006edbb964602e4ec7ec58
DustinSeltz <56089768+DustinSeltz@users.noreply.github.com>
2019-10-15 14:24:38 -0700

    Test. Changed the text of Hello World.

--- app/src/main/java/com/harmony/livecolor/MainActivity.java
+++ app/src/main/java/com/harmony/livecolor/MainActivity.java
@@ -3,6 +3,8 @@ package com.harmony.livecolor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.TextView;
 
 // MAIN ACTIVITY - COLOR PICKER
 // [See the designs on our marvel for creating and implementing UI]
@@ -15,5 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //TODO how do we treat different actions?
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        float x = event.getX();
+        float y = event.getY();
+        //TODO use that to get color of an image.
+        //TODO Maybe zoom in so that they can select a single color, or suggest several nearby colors.
+        ((TextView) findViewById(R.id.helloWorldBox)).setText("Detected press at x="+x+" y="+y);
+        return true;
     }
 }
