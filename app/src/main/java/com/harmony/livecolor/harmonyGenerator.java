a6e3e0301d8771c5642fee38feff3d6f5c60d8d4
DustinSeltz <56089768+DustinSeltz@users.noreply.github.com>
2019-11-21 14:58:55 -0800

    Added some code to test harmonies by putting them into palettes. Unfinished.

--- app/src/main/java/com/harmony/livecolor/harmonyGenerator.java
+++ app/src/main/java/com/harmony/livecolor/harmonyGenerator.java
@@ -2,6 +2,11 @@ package com.harmony.livecolor;
 
 import android.util.Log;
 
+import java.util.ArrayList;
+
+import static android.graphics.Color.RGBToHSV;
+
+//Contains functions for generating colors for palettes based on a given color.
 public class harmonyGenerator {
 
     //TODO Determine what input we want to support. Can overload I guess.
@@ -11,6 +16,59 @@ public class harmonyGenerator {
     // Helper functions if I need decide on a different format.
 
     //TODO tests
+
+    //Each color[] should be the hsv like what is returned by the rest of the functions in this file.
+    public static ArrayList<MyColor> colorsToMyColors(int[][] colors, int numberOfColors){
+        ArrayList<MyColor> myColorArrayList = new ArrayList<>();
+        for(int i = 0; i < numberOfColors; ++i){
+            //myColorArrayList.push_back();
+        }
+        return myColorArrayList;
+    }
+
+    public static MyColor hsvToMyColor(int[] color, int id){
+        String hex = hsvToStringHex(color);
+        String rgb = hsvToStringRgb(color);
+        String hsv = hsvToStringHsv(color);
+        MyColor colorObj = new MyColor(""+id, ("Test"+id), hex, rgb, hsv);
+        return colorObj;
+    }
+    //For all these functions, currently I'm using hue (0..359) saturation (0..100) value (0..100)
+
+    //Returns something like "#FFFFFF"
+    public static String hsvToStringHex(int[] color){
+        //Easiest way to get hex is probably to convert to decimal rgb, and then to hex.
+        int hue = color[0];
+        int saturation = color[1];
+        int value = color[2];
+        int[] rgb = EditColorActivity.convertHSVtoRGB(hue, saturation, value);
+        //https://stackoverflow.com/a/3607942
+        String hex = String.format("#%02x%02x%02x", rgb[0], rgb[1], rgb[2]);
+        return "#"+hex;
+    }
+
+    //Returns something like "(0, 0 ,0)"
+    public static String hsvToStringRgb(int[] color){
+        int hue = color[0];
+        int saturation = color[1];
+        int value = color[2];
+        int[] rgb = EditColorActivity.convertHSVtoRGB(hue, saturation, value);
+        String strRgb = "("+rgb[0]+", "+rgb[1]+", "+rgb[2]+")";
+        return strRgb;
+    }
+    //TODO need to decide on a format for this.
+    public static String hsvToStringHsv(int[] color){
+        //Easiest way to get proper hsv is to use the built in function like we did in ColorPickerFragment.
+        int hue = color[0];
+        int saturation = color[1];
+        int value = color[2];
+        int[] rgb = EditColorActivity.convertHSVtoRGB(hue, saturation, value);
+        //Stores the result values
+        float[] hsvArray = new float[3];
+        RGBToHSV(rgb[0],rgb[1],rgb[2],hsvArray);
+        //TODO decide on a format for the string.
+        return "TODO harmonyGenerator.hsvToStringHsv()";
+    }
 
     //Complement is just the opposite hue
     public static int[] complement(int hue, int saturation, int value){
