bcd59eeebbc796fbf9bac35371452a7e88829152
Andrew <andrewtgg@yahoo.com>
2019-11-26 14:22:16 -0800

    camera potential fix

--- app/src/main/java/com/harmony/livecolor/ColorPickerFragment.java
+++ app/src/main/java/com/harmony/livecolor/ColorPickerFragment.java
@@ -216,19 +216,19 @@ public class ColorPickerFragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode == Activity.RESULT_OK && data != null) {
+            pickingImage.setImageURI(imageUri); // updates the view
+        }
             if(requestCode == RESULT_LOAD_IMAGE) {
                 imageUri = data.getData(); // only needed for gallery images
-            }
-            pickingImage.setImageURI(imageUri); // updates the view
-            imagePath = getPath(data, this.getActivity());
-            if (imagePath != null) {
-                // save image path to saved prefs after updating imageview
-                SharedPreferences.Editor editor = getContext().getSharedPreferences("prefs", MODE_PRIVATE).edit();
-                editor.putString("image", imagePath);
-                editor.apply();
+                imagePath = getPath(data, this.getActivity());
+                if (imagePath != null) {
+                    // save image path to saved prefs after updating imageview
+                    SharedPreferences.Editor editor = getContext().getSharedPreferences("prefs", MODE_PRIVATE).edit();
+                    editor.putString("image", imagePath);
+                    editor.apply();
+                }
             }
         }
-    }
     // gets path of image to save to fragment
     public String getPath(Intent data, Context context) {
         String[] projection = { MediaStore.Images.Media.DATA };
