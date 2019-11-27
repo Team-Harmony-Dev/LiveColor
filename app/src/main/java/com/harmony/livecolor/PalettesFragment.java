b52c37664a8fa4687e41320b32738427d8ab5545
DustinSeltz <56089768+DustinSeltz@users.noreply.github.com>
2019-11-26 16:32:07 -0800

    Reset palettes to normal dummy code.

--- app/src/main/java/com/harmony/livecolor/PalettesFragment.java
+++ app/src/main/java/com/harmony/livecolor/PalettesFragment.java
@@ -72,7 +72,7 @@ public class PalettesFragment extends Fragment {
         //will access palettes from database and put into MyPalette objects
         //TODO: Andrew's database code/method call will go here
         //Temporary Palettes atm:
-        /*
+
         MyColor magenta = new MyColor("1","Hot Pink", "#FF00FF", "(255, 0, 255)","(5:001, 255, 255)");
         MyColor yellow = new MyColor("2","Highlighter", "#FFFF00", "(255, 255, 0)","(1:001, 255, 255)");
         MyColor cyan = new MyColor("3","Hot Cyan", "#00FFFF", "(0, 255, 255)","(3:001, 255, 255)");
@@ -106,33 +106,6 @@ public class PalettesFragment extends Fragment {
         colorList3.add(yellow);
         colorList3.add(cyan);
         paletteList.add(new MyPalette("3","Ten+ Colors",colorList3));
-        */
-
-        //TODO should probably have a helper function for these tests.
-        //Note: fragment_palettes.xml limits the number of colors displayed per palette on the menu to 10.
-        // also the edit button when you select a palette does nothing.
-
-        int[][] testBasicColor = new int[][] {new int[] {180, 100, 100}};
-        ArrayList<MyColor> testBasicColorMyColors = harmonyGenerator.colorsToMyColors(testBasicColor, 1);
-        MyPalette testBasicColorPalette = new MyPalette("1", "Single color", testBasicColorMyColors);
-        paletteList.add(testBasicColorPalette);
-        //Testing getting the analogous colors 15 degrees to each side of the given color ().
-        int[][] testAnalogous = harmonyGenerator.analogousScheme(180, 100, 100, 20, 5);
-        ArrayList<MyColor> testAnalogousMyColors = harmonyGenerator.colorsToMyColors(testAnalogous, 5);
-        MyPalette testAnalogousPalette = new MyPalette("1", "Analogous", testAnalogousMyColors);
-        paletteList.add(testAnalogousPalette);
-        //Testing getting the analogous colors 15 degrees to each side of the given color ().
-        int[][] testMonochromatic = harmonyGenerator.monochromaticScheme(180, 100, 100, 50, 3);
-        ArrayList<MyColor> testMonochromaticMyColors = harmonyGenerator.colorsToMyColors(testMonochromatic, 3);
-        MyPalette testMonochromaticPalette = new MyPalette("2", "Monochromatic", testMonochromaticMyColors);
-        paletteList.add(testMonochromaticPalette);
-        //TODO triadic test
-
-        //Testing getting the analogous colors 15 degrees to each side of the given color ().
-        int[][] testEvenEvenSpaced = harmonyGenerator.evenlySpacedScheme(300, 100, 100, 4);
-        ArrayList<MyColor> testEvenEvenSpacedMyColors = harmonyGenerator.colorsToMyColors(testEvenEvenSpaced, 4);
-        MyPalette testEvenEvenSpacedPalette = new MyPalette("4", "Four evenly spaced on color wheel", testEvenEvenSpacedMyColors);
-        paletteList.add(testEvenEvenSpacedPalette);
     }
 
     public void initRecycler(){
