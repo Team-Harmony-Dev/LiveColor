b5102ce15254cb22ce4436829dcc66f87df4480b
DustinSeltz <56089768+DustinSeltz@users.noreply.github.com>
2020-06-27 15:55:09 -0700

    It compiles now.

--- app/src/main/java/com/harmony/livecolor/ColorNameGetterCSV.java
+++ app/src/main/java/com/harmony/livecolor/ColorNameGetterCSV.java
@@ -1,5 +1,7 @@
 package com.harmony.livecolor;
 
+import android.content.Context;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,12 +12,27 @@ import java.util.List;
 //So many probs with API, try CSV instead.
 //TODO load from res/raw/colornames.csv
 //https://stackoverflow.com/questions/38415680/how-to-parse-csv-file-into-an-array-in-android-studio#38415815
-public class ColorNameGetterCSV {
+public class ColorNameGetterCSV extends android.app.Application { //TODO extends gives context for getresources? Is that right?
 
     private InputStream inputStream;
     //Might be redundant
     private boolean haveAlreadyReadNames = false;
     private List colorNames;
+
+    /*
+    //https://stackoverflow.com/a/8238658
+    private static ColorNameGetterCSV mApp = null;
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        mApp = this;
+    }
+    public static Context context()
+    {
+        return mApp.getApplicationContext();
+    }
+     */
 
     public ColorNameGetterCSV(InputStream inputStream){
         this.inputStream = inputStream;
@@ -48,9 +65,8 @@ public class ColorNameGetterCSV {
     private void readColors(){
         InputStream inputStream = getResources().openRawResource(R.raw.colornames);
         ColorNameGetterCSV colors = new ColorNameGetterCSV(inputStream);
-        List colorList = colors.read();
         //TODO is this the best way to do this?
-        this.colorNames = colorList;
+        this.colorNames = colors.read();
     }
 
     public String getName(){
