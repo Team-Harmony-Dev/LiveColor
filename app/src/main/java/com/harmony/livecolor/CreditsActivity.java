7abadb1aac44b5de7d9f5d3272302f8f9a8956f4
tielm1997 <50973899+tielm1997@users.noreply.github.com>
2020-07-11 17:30:16 -0700

    Scrollable version. Still need to update picture sizes, and add other members.

--- app/src/main/java/com/harmony/livecolor/CreditsActivity.java
+++ app/src/main/java/com/harmony/livecolor/CreditsActivity.java
@@ -10,20 +10,15 @@ import android.widget.TextView;
 
 public class CreditsActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private static final String TAG = "Whatever the heck";
-    TextView mtv;
+    //private static final String TAG = "Whatever the heck";
+    //TextView mtv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
-        mtv = findViewById(R.id.helloWorldText);
+        //mtv = findViewById(R.id.helloWorldText);
         //Button myB = findViewById(R.id.my_button);
         //myB.setOnClickListener(this);
-    }
-
-    public void buttonOnClick(View view){
-        mtv.setText(":)");
-        Log.d(TAG, "This thing dont work");
     }
 
     @Override
