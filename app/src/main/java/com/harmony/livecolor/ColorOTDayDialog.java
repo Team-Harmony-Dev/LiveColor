c0e5dd00567983e8481cad3278b017c8ff85021a
glindsey22 <56328806+glindsey22@users.noreply.github.com>
2020-07-20 16:53:03 -0700

    Initial work on shine animation on dialog.

--- app/src/main/java/com/harmony/livecolor/ColorOTDayDialog.java
+++ app/src/main/java/com/harmony/livecolor/ColorOTDayDialog.java
@@ -4,7 +4,11 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,12 +17,15 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
 
+import static com.harmony.livecolor.UsefulFunctions.getIntFromColor;
+
 public class ColorOTDayDialog {
     int colorOfTheDay;
     Date todayDate;
     Context context;
     Activity activity;
     AlertDialog alertDialogSave;
+    View colorOTDView;
 
     public ColorOTDayDialog(Context context) {
         this.context = context;
@@ -27,10 +34,10 @@ public class ColorOTDayDialog {
 
     public void showColorOTD() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this.context);
-        View colorOTDView = activity.getLayoutInflater().inflate(R.layout.dialog_color_day,null);
+        colorOTDView = activity.getLayoutInflater().inflate(R.layout.dialog_color_day,null);
 
         //Get/Set the current date
-        Date todayDate = Calendar.getInstance().getTime();
+        todayDate = Calendar.getInstance().getTime();
         TextView dateView = (TextView) colorOTDView.findViewById(R.id.dateView);
         SimpleDateFormat simpleDate =  new SimpleDateFormat("MM/dd/yyyy");
         String strDt = simpleDate.format(todayDate);
@@ -47,7 +54,12 @@ public class ColorOTDayDialog {
 
         builder.setView(colorOTDView);
         alertDialogSave = builder.create();
-        alertDialogSave.show();
+        if(newDay()){
+            alertDialogSave.show();
+            makeShine();
+            //ImageView shineView = colorOTDView.findViewById(R.id.shine);
+            //shineView.setImageAlpha(0);
+        }
     }
 
     public int generateRandomColor(){
@@ -56,7 +68,22 @@ public class ColorOTDayDialog {
         int randGreen = rand.nextInt(256);
         int randBlue = rand.nextInt(256);
         //return getIntFromColor(randRed, randGreen, randBlue);
-        return UsefulFunctions.getIntFromColor(randRed, randGreen, randBlue);
+        return getIntFromColor(randRed, randGreen, randBlue);
     }
 
+    public boolean newDay(){
+        return true;
+    }
+
+    public void makeShine(){
+        ImageView colorView = colorOTDView.findViewById(R.id.colorOTDView);
+        ImageView shineView = colorOTDView.findViewById(R.id.shine);
+        String toX = Integer.toString(colorView.getWidth()+shineView.getWidth());
+        Log.d("toXDelta is", toX);
+        Animation animation = new TranslateAnimation(-120, 200,0, 0);
+        animation.setDuration(1550);
+        animation.setFillAfter(true);
+        animation.setInterpolator(new AccelerateDecelerateInterpolator());
+        shineView.startAnimation(animation);
+    }
 }
